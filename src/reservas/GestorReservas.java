920f217985cd60eb4a8bd51a29a83e9d8eee76e7
Cristianet19 <cristian19herrero@gmail.com>
2025-09-13 23:11:31 +0200

    segundo push

--- src/reservas/GestorReservas.java
+++ src/reservas/GestorReservas.java
@@ -251,8 +251,13 @@ public class GestorReservas {
 	 */
 	@SuppressWarnings("unchecked")
 	public JSONArray listaPlazasDisponibles(String actividad) {
-        // POR IMPLEMENTAR
-        return null; // MODIFICAR
+        JSONArray SesionesDisponibles = new JSONArray();
+        for(DiaSemana dia : sesionesSemana.keySet()) {
+        	for(Sesion se : sesionesSemana.get(dia)) {
+        		if(se.getPlazas() != 0) SesionesDisponibles.add(se.toJSON());
+        	}
+        }
+        return SesionesDisponibles;// MODIFICAR
 	}
 
 
@@ -300,7 +305,10 @@ public class GestorReservas {
 	 * @return La reserva encontrada o `null` si no existe una reserva con ese código.
 	 */
 	private Reserva buscaReserva(Vector<Reserva> vector, long codReserva) {
-        // POR IMPLEMENTAR
+        if(!reservas.containsKey(codReserva)) return null;
+        for(Reserva re : vector) {
+        	if(re.getCodReserva() == codReserva) return re;
+        }
         return null; // MODIFICAR
 	}
 
